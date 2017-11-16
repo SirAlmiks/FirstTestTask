c4c5d28ec14585945e5247c265290633421a5b42
Almiks <kingalmiks@gmail.com>
2017-11-16 02:27:20 +0200

    Added explosions effects Added sound effects and music (music increases volum by time, like on video) Added and calibrated animation events (effects and sound) Set up frost sword effects Set up camera position Preparing to test and produce all bunch to Android dev (Yay, i did it if 5 days! >D )

---
+++ Assets/Scripts/HitDroidEvents.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitDroidEvents : MonoBehaviour {
+
+	private GameObject
+	droneHit,
+	droneDead,
+	clone1, clone2;
+	//prefabs
+	private Transform drone; //coordinates of drone
+
+	void Start()
+	{
+		droneHit = Resources.Load("Prefabs/ExplosionHit") as GameObject;
+		droneDead = Resources.Load("Prefabs/ExplosionDead") as GameObject;
+		drone = GameObject.FindGameObjectWithTag ("Drone").GetComponent<Transform> ();
+	}
+	public void Hit()
+	{
+		clone1 = Instantiate(droneHit, drone.position, drone.rotation, drone) as GameObject; 
+	}
+
+	public void Boom()
+	{
+		clone2 = Instantiate(droneDead, drone.position, drone.rotation, drone) as GameObject;
+	}
+
+	public void Clear()
+	{
+		Destroy (clone1);
+		Destroy (clone2);
+	}
+}
---
+++ Assets/SoundsManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsManager : MonoBehaviour {
+
+
+	private AudioSource
+	music,
+	effects;
+
+	public AudioClip[] sounds;
+
+	void Start () {
+		music = GameObject.Find ("BackgroundMusic").GetComponent<AudioSource> ();
+		effects = GameObject.Find ("SoundEffects").GetComponent<AudioSource> ();
+			
+	}
+
+	void Update () {
+		if (music.volume < 0.6f)
+		music.volume += 0.001f;
+	}
+
+	public void SoundAppearance()
+	{
+		effects.PlayOneShot (sounds[0]);
+	}
+
+	public void SoundHit1()
+	{
+		effects.PlayOneShot (sounds[1]);
+	}
+
+	public void SoundHit2()
+	{
+		effects.PlayOneShot (sounds[2]);
+	}
+
+	public void SoundIdle()
+	{
+		effects.clip = sounds[3];
+		effects.Play();
+	}
+}
