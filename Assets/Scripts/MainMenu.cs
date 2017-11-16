6c22ff01d4f0810b935d67e107df75b03ff6148c
Almiks <kingalmiks@gmail.com>
2017-11-17 01:57:36 +0200

    Last check outs and little updates on project Added Tap on screen event Tested build project All Done! :D

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	private GameObject
+	MainAnim,
+	UIText;
+	bool isStart = false; //проверка - началось ли уже действие
+
+	void Start () {
+		MainAnim = Resources.Load("Prefabs/CV_BT_ROBOT_07") as GameObject;
+		UIText = GameObject.Find("UI");
+	}
+	
+	void Update () {		
+		
+		if (Input.touchCount > 0 && !isStart)
+		{
+			GameObject	clone = Instantiate(MainAnim) as GameObject;
+			Destroy (UIText);
+			isStart = true;
+		}
+	}
+	
+	}
